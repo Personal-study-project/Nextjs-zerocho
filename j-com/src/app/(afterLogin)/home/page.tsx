e86d5283dfe694eeae04bfad3a8d87ae6275d2da
jae-hwan-kim <woghks7859@gmail.com>
2024-03-06 19:01:24 +0900

    feat: dayjs 사용하여 게시글 업로드 시간 나타내기

--- j-com/src/app/(afterLogin)/home/page.tsx
+++ j-com/src/app/(afterLogin)/home/page.tsx
@@ -2,6 +2,7 @@ import style from './home.module.css';
 import Tab from '@/app/(afterLogin)/home/_component/Tab';
 import TabProvider from '@/app/(afterLogin)/home/_component/TabProvider';
 import PostForm from './_component/PostForm';
+import Post from '../_component/Post';
 
 export default function Page() {
     return (
@@ -9,7 +10,11 @@ export default function Page() {
             <TabProvider>
                 <Tab />
                 <PostForm />
-                {/* <Post /> */}
+                <Post />
+                <Post />
+                <Post />
+                <Post />
+                <Post />
             </TabProvider>
         </main>
     );
