31983bfd2ede0b9f04c59dc13dbecd328e7618d3
jae-hwan-kim <woghks7859@gmail.com>
2024-03-06 18:41:50 +0900

    feat: Context API 활용해서 상태관리 및 Tab 구현

--- j-com/src/app/(afterLogin)/home/page.tsx
+++ j-com/src/app/(afterLogin)/home/page.tsx
@@ -1,7 +1,16 @@
+import style from './home.module.css';
+import Tab from '@/app/(afterLogin)/home/_component/Tab';
+import TabProvider from '@/app/(afterLogin)/home/_component/TabProvider';
+import PostForm from './_component/PostForm';
+
 export default function Page() {
     return (
-        <main>
-            홈페이지
+        <main className={style.main}>
+            <TabProvider>
+                <Tab />
+                <PostForm />
+                {/* <Post /> */}
+            </TabProvider>
         </main>
     );
 }
