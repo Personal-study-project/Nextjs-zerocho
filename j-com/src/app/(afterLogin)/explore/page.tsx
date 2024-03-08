226944ebf2e1c001e8dff3968d4dd6279f175017
jae-hwan-kim <woghks7859@gmail.com>
2024-03-08 12:06:35 +0900

    feat: 탐색하기 페이지 구현

--- j-com/src/app/(afterLogin)/explore/page.tsx
+++ j-com/src/app/(afterLogin)/explore/page.tsx
@@ -1,7 +1,22 @@
+import SearchForm from "../_component/SearchFrom";
+import Trend from "../_component/Trend";
+import style from './explore.module.css';
+
 export default function Page() {
     return (
-        <div>
-            <h1>explore</h1>
-        </div>
+        <main className={style.main}>
+            <div className={style.formZone}>
+                <SearchForm />
+            </div>
+            <div className={style.trend}>
+                <h3>나를 위한 트랜드</h3>
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+                <Trend />
+            </div>
+        </main>
     );
 }
