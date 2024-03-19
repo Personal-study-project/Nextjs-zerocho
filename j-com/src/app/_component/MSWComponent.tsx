33493a36fbac6e7a3e23c6038d4b0548e60749bf
jae-hwan-kim <woghks7859@gmail.com>
2024-03-19 17:18:03 +0900

    feat: msw 사용하여 테스트환경 테스트 & 리팩토링

--- j-com/src/app/_component/MSWComponent.tsx
+++ j-com/src/app/_component/MSWComponent.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export const MSWComponent = () => {
     useEffect(() => {
-        if (typeof window !== "undefined") {
+        if (typeof window !== "undefined") { // 클라이언트 환경일 때
             if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
                 require("@/mocks/browser");
             }
